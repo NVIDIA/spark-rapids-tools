c21e54eae266586fcc524e174ce072f5531b66b6
liyuan <yuali@nvidia.com>
2023-03-11 15:04:56 +0800

    data validation test
    Signed-off-by: liyuan <yuali@nvidia.com>

--- user_tools/src/spark_rapids_dataproc_tools/data_validation.py
+++ user_tools/src/spark_rapids_dataproc_tools/data_validation.py
@@ -25,67 +25,69 @@ import pkg_resources
 # from spark_rapids_dataproc_tools.utilities import get_log_dict, run_cmd
 
 # Setup logging
-# logger = logging.getLogger('diag')
+logger = logging.getLogger('validation')
 #
 # consoleHandler = logging.StreamHandler(sys.stdout)
 # logFormatter = logging.Formatter('%(message)s')
 # consoleHandler.setFormatter(logFormatter)
 # logger.addHandler(consoleHandler)
 #
-# logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
 
 
 class Validation:
     """Data Validation tool basic class."""
    #        validate = Validation(cluster, region, check, format, t1, t1p, t2, t2p, pk, e, i, f, o, of, p, debug)
 
-    def __init__(self, init_number, cpu_table_path, gpu_table_path):
-        self.init_number = init_number
-        self.result = self.init_number
-        self.cpu_table_path = cpu_table_path
-        self.gpu_table_path = gpu_table_path
+    def __init__(self, debug=False):
+        if debug:
+            logger.setLevel(logging.DEBUG)
+
+        # Diagnostic summary
+        self.summary = {}
+
 
     # work
-    def add2(self):
-        print(self.cpu_table_path)
-        print(self.gpu_table_path)
-        print("-----add2---")
-
-    def compare(self, cpu_df, gpu_df):
-        #
-        print(cpu_df.count)
-        print(gpu_df.count)
-        print("--------compare------")
-
-    def compare_result(self):
-        # create spark session
-        spark = (SparkSession
-                 .builder
-                 .master("spark://yuanli-System-Product-Name:7077")
-                 .appName("data-validation-tool")
-                 .getOrCreate())
-#.appName(args.mainClass)
-
-        # load to dataframe /home/yuanli/work/project/data-validation/test-dataset/cpu_demo
-        df_cpu = spark.read.format("parquet").load(self.cpu_table_path)
-        df_gpu = spark.read.format("parquet").load(self.gpu_table_path)
-
-        self.compare(df_cpu, df_gpu)
-
-    def compare_result_gcs(self):
-        # create spark session
-        spark = (SparkSession
-                 .builder
-                 .master("yarn")
-                 .appName("data-validation-tool")
-                 .getOrCreate())
-#.appName(args.mainClass)
-
-        # load to dataframe /home/yuanli/work/project/data-validation/test-dataset/cpu_demo
-        df_cpu = spark.read.format("parquet").load(self.cpu_table_path)
-        df_gpu = spark.read.format("parquet").load(self.gpu_table_path)
-
-        self.compare(df_cpu, df_gpu)
+#     def add2(self):
+#         print(self.cpu_table_path)
+#         print(self.gpu_table_path)
+#         print("-----add2---")
+#
+#     def compare(self, cpu_df, gpu_df):
+#         #
+#         print(cpu_df.count)
+#         print(gpu_df.count)
+#         print("--------compare------")
+#
+#     def compare_result(self):
+#         # create spark session
+#         spark = (SparkSession
+#                  .builder
+#                  .master("spark://yuanli-System-Product-Name:7077")
+#                  .appName("data-validation-tool")
+#                  .getOrCreate())
+# #.appName(args.mainClass)
+#
+#         # load to dataframe /home/yuanli/work/project/data-validation/test-dataset/cpu_demo
+#         df_cpu = spark.read.format("parquet").load(self.cpu_table_path)
+#         df_gpu = spark.read.format("parquet").load(self.gpu_table_path)
+#
+#         self.compare(df_cpu, df_gpu)
+#
+#     def compare_result_gcs(self):
+#         # create spark session
+#         spark = (SparkSession
+#                  .builder
+#                  .master("yarn")
+#                  .appName("data-validation-tool")
+#                  .getOrCreate())
+# #.appName(args.mainClass)
+#
+#         # load to dataframe /home/yuanli/work/project/data-validation/test-dataset/cpu_demo
+#         df_cpu = spark.read.format("parquet").load(self.cpu_table_path)
+#         df_gpu = spark.read.format("parquet").load(self.gpu_table_path)
+#
+#         self.compare(df_cpu, df_gpu)
 
 
 def main():
