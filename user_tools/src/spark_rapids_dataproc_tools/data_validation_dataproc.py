f388853aa66f0c7d49b4e741f20adc89a54bd01d
liyuan <yuali@nvidia.com>
2023-03-14 12:45:18 +0800

    metadata validation test
    Signed-off-by: liyuan <yuali@nvidia.com>

--- user_tools/src/spark_rapids_dataproc_tools/data_validation_dataproc.py
+++ user_tools/src/spark_rapids_dataproc_tools/data_validation_dataproc.py
@@ -82,7 +82,7 @@ class DataValidationDataproc(Validation):
         if self.f is None:
             filters = 'None'
         else:
-            filters = filters.replace('\'', '\\\'')
+            filters = self.f.replace('\'', '\\\'')
 
         # print(self.convert_tuple_to_string(self.i))
         compare_job = {
