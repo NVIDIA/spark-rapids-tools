6d808738ba7c8e0bd69c1acdb3a2ab52ab763579
Matt Ahrens <matthewahrens@gmail.com>
2023-04-04 09:30:41 -0500

    Fixing style issue with unrelated code
    Signed-off-by: Matt Ahrens <matthewahrens@gmail.com>

--- core/src/main/scala/org/apache/spark/sql/rapids/tool/qualification/QualificationAppInfo.scala
+++ core/src/main/scala/org/apache/spark/sql/rapids/tool/qualification/QualificationAppInfo.scala
@@ -729,7 +729,8 @@ object QualificationAppInfo extends Logging {
   // Summarize and estimate based on wall clock times
   def calculateEstimatedInfoSummary(estimatedRatio: Double, sqlDataFrameDuration: Long,
       appDuration: Long, sqlSpeedupFactor: Double, appName: String,
-      appId: String, hasFailures: Boolean, mlSpeedupFactor: Option[MLFuncsSpeedupAndDuration] = None,
+      appId: String, hasFailures: Boolean,
+      mlSpeedupFactor: Option[MLFuncsSpeedupAndDuration] = None,
       unsupportedExecs: String = "", unsupportedExprs: String = "",
       allClusterTagsMap: Map[String, String] = Map.empty[String, String]): EstimatedSummaryInfo = {
     val sqlDataFrameDurationToUse = if (sqlDataFrameDuration > appDuration) {
