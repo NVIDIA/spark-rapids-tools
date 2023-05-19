ef26c9070f8321f1e77f856cd197f7bd5297a1bc
Cindy Jiang <cindyj@nvidia.com>
2023-05-19 12:07:43 -0700

    refactored AzureStorageDriver to take abfs paths and updated implementation to use azure cli
    Signed-off-by: Cindy Jiang <cindyj@nvidia.com>

--- user_tools/src/spark_rapids_pytools/cloud_api/azurestorage.py
+++ user_tools/src/spark_rapids_pytools/cloud_api/azurestorage.py
@@ -17,6 +17,7 @@
 from dataclasses import dataclass
 
 from spark_rapids_pytools.cloud_api.sp_types import CMDDriverBase
+from spark_rapids_pytools.common.prop_manager import JSONPropertiesContainer
 from spark_rapids_pytools.common.sys_storage import StorageDriver, FSUtil
 
 
@@ -29,34 +30,58 @@ class AzureStorageDriver(StorageDriver):
 
     @classmethod
     def get_cmd_prefix(cls):
-        pref_arr = ['azcopy']
+        pref_arr = ['az storage fs']
         return pref_arr[:]
 
+    @classmethod
+    def get_file_system(cls, url: str):
+        return url.split('@')[0].split('://')[1]
+
+    @classmethod
+    def get_account_name(cls, url: str):
+        return url.split('@')[1].split('.')[0]
+
+    @classmethod
+    def get_path(cls, url: str):
+        return url.split('dfs.core.windows.net')[1]
+
     def resource_is_dir(self, src: str) -> bool:
-        if not src.startswith('https://'):
+        if not src.startswith('abfss://'):
             return super().resource_is_dir(src)
         cmd_args = self.get_cmd_prefix()
-        # TODO: append SAS token
-        cmd_args.extend(['list', src])
+
+        file_system = self.get_file_system(src)
+        account_name = self.get_account_name(src)
+        path = self.get_path(src)
+
+        cmd_args.extend(['file list', '-f', file_system, '--account-name', account_name])
+        if len(path) > 0:
+            cmd_args.extend(['--path', path])
+
         try:
             std_out = self.cli.run_sys_cmd(cmd_args)
-            stdout_lines = std_out.splitlines()
-            if stdout_lines:
-                for out_line in stdout_lines:
-                    if src in out_line:
-                        # if any path contains the directory path return True
-                        return True
+            stdout_info = JSONPropertiesContainer(prop_arg=std_out, file_load=False)
+            if len(stdout_info.props) != 1:
+                # not a file
+                return True
         except RuntimeError:
             self.cli.logger.debug('Error in checking resource [%s] is directory', src)
         return False
 
     def resource_exists(self, src) -> bool:
-        if not src.startswith('https://'):
+        if not src.startswith('abfss://'):
             return super().resource_exists(src)
-        # run 'azcopy list src' if result is 0, then the resource exists.
+        # run 'az storage fs file list' if result is 0, then the resource exists.
         cmd_args = self.get_cmd_prefix()
-        # TODO: append SAS token
-        cmd_args.extend(['list', src])
+
+        file_system = self.get_file_system(src)
+        account_name = self.get_account_name(src)
+        path = self.get_path(src)
+
+        cmd_args.extend(['file list', '-f', file_system, '--account-name', account_name])
+        if len(path) > 0:
+            cmd_args.extend(['--path', path])
+
         # run command and make sure we return 0.
         try:
             self.cli.run_sys_cmd(cmd_args)
@@ -66,25 +91,47 @@ class AzureStorageDriver(StorageDriver):
         return res
 
     def _download_remote_resource(self, src: str, dest: str) -> str:
-        if not src.startswith('https://'):
+        if not src.startswith('abfss://'):
             return super()._download_remote_resource(src, dest)
         # this is azure data lake storage
+        file_system = self.get_file_system(src)
+        account_name = self.get_account_name(src)
+        path = self.get_path(src)
+
         cmd_args = self.get_cmd_prefix()
-        # TODO: append SAS token
-        cmd_args.extend(['cp', src, dest])
+
         if self.resource_is_dir(src):
-            cmd_args.append('--recursive=true')
+            cmd_args.extend(['directory download', '-f', file_system, '--account-name', account_name])
+            cmd_args.extend(['-s', path, '-d', dest, '--recursive'])
+        else:
+            cmd_args.extend(['file download', '-f', file_system, '--account-name', account_name])
+            cmd_args.extend(['-p', path, '-d', dest])
         self.cli.run_sys_cmd(cmd_args)
         return FSUtil.build_full_path(dest, FSUtil.get_resource_name(src))
 
     def _upload_remote_dest(self, src: str, dest: str, exclude_pattern: str = None) -> str:
-        if not dest.startswith('https://'):
+        if not dest.startswith('abfss://'):
             return super()._upload_remote_dest(src, dest)
         # this is azure data lake storage
+        file_system = self.get_file_system(dest)
+        account_name = self.get_account_name(dest)
+        path = self.get_path(dest)
+
         cmd_args = self.get_cmd_prefix()
-        cmd_args.extend(['cp', src, dest])
+
         if self.resource_is_dir(src):
-            cmd_args.append('--recursive=true')
+            cmd_args.extend(['directory upload', '-f', file_system, '--account-name', account_name])
+            cmd_args.extend(['-s', src, '-d', path, '--recursive'])
+        else:
+            if self.resource_is_dir(dest):
+                if not path[-1] == '/':
+                    path = path + '/'
+                path = path + FSUtil.get_resource_name(src)
+                cmd_args.extend(['directory upload', '-f', file_system, '--account-name', account_name])
+                cmd_args.extend(['-s', src, '-d', path])
+            else:
+                cmd_args.extend(['file upload', '-f', file_system, '--account-name', account_name])
+                cmd_args.extend(['-s', src, '-p', path])
         self.cli.run_sys_cmd(cmd_args)
         return FSUtil.build_path(dest, FSUtil.get_resource_name(src))
 
@@ -94,11 +141,20 @@ class AzureStorageDriver(StorageDriver):
         return super().is_file_path(value)
 
     def _delete_path(self, src, fail_ok: bool = False):
-        if not src.startswith('https://'):
+        if not src.startswith('abfss://'):
             super()._delete_path(src)
         else:
             cmd_args = self.get_cmd_prefix()
-            cmd_args.extend(['rm', src])
+            # this is azure data lake storage
+            file_system = self.get_file_system(src)
+            account_name = self.get_account_name(src)
+            path = self.get_path(src)
+
             if self.resource_is_dir(src):
-                cmd_args.append('--recursive')
+                cmd_args.extend(['directory delete', '-f', file_system, '--account-name', account_name])
+                cmd_args.extend(['-n', path, '-y'])
+            else:
+                cmd_args.extend(['file delete', '-f', file_system, '--account-name', account_name])
+                cmd_args.extend(['-p', path, '-y'])
+
             self.cli.run_sys_cmd(cmd_args)
