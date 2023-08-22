56db28181c66fdcd8a2956864b46cd2dc7f24dec
Ahmed Hussein (amahussein) <a@ahussein.me>
2023-08-21 21:45:40 -0500

    Fix unit-tests for Spark-340
    Signed-off-by: Ahmed Hussein (amahussein) <a@ahussein.me>
    
    Fixes #498, Fixes #463
    
    - Update the gh-workflow to run mvn for list of spark-versions
    - Fixes the delta-lake dependency in the pom.xml file
    - Fix the broken unit test for spark-340
    - Update the spark-341 to point to release instead of snapshot
    - Fix the Autotuner warning

--- core/src/test/scala/com/nvidia/spark/rapids/tool/qualification/QualificationSuite.scala
+++ core/src/test/scala/com/nvidia/spark/rapids/tool/qualification/QualificationSuite.scala
@@ -1154,7 +1154,8 @@ class QualificationSuite extends BaseTestSuite {
         try {
           val lines = inputSource.getLines.toSeq
           // 1 for header, 1 for values
-          assert(lines.size == 6)
+          val expLinesSize = if (ToolUtils.isSpark340OrLater()) 8 else 6
+          assert(lines.size == expLinesSize)
           assert(lines.head.contains("App ID,Unsupported Type,"))
           assert(lines(1).contains("\"Read\",\"JSON\",\"Types not supported - bigint:int\""))
         } finally {
