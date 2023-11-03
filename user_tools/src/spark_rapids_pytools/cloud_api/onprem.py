30d8381d290c87b18cd0a490dbc517f7bf1bba32
mattahrens <matthewahrens@gmail.com>
2023-11-03 09:40:26 -0500

    Fixing flake issue with whitespace
    Signed-off-by: mattahrens <matthewahrens@gmail.com>

--- user_tools/src/spark_rapids_pytools/cloud_api/onprem.py
+++ user_tools/src/spark_rapids_pytools/cloud_api/onprem.py
@@ -312,7 +312,7 @@ class OnpremSavingsEstimator(SavingsEstimator):
         if self.price_provider.name.casefold() == 'dataproc':
             master_cost = self.__calculate_dataproc_group_cost(cluster, SparkNodeType.MASTER)
             workers_cost = self.__calculate_dataproc_group_cost(cluster, SparkNodeType.WORKER)
-            master_cores = (cluster.get_nodes_cnt(SparkNodeType.MASTER) 
+            master_cores = (cluster.get_nodes_cnt(SparkNodeType.MASTER)
                             * cluster.get_node_core_count(SparkNodeType.MASTER))
             worker_cores = (cluster.get_nodes_cnt(SparkNodeType.WORKER)
                             * cluster.get_node_core_count(SparkNodeType.WORKER))
