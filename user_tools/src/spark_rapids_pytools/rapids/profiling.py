724f5653d2fea21617c4f66e820153b2cafd98b2
cindyyuanjiang <cindyj@nvidia.com>
2023-12-07 11:07:25 -0800

    refactored code
    Signed-off-by: cindyyuanjiang <cindyj@nvidia.com>

--- user_tools/src/spark_rapids_pytools/rapids/profiling.py
+++ user_tools/src/spark_rapids_pytools/rapids/profiling.py
@@ -247,7 +247,7 @@ class Profiling(RapidsJarTool):
         self.__generate_report_with_recommendations()
 
     def _init_rapids_arg_list(self) -> List[str]:
-        platform_arg = ['--platform', self.ctxt.platform.get_platform_name().replace('_', '-')]
+        platform_arg = super()._init_rapids_arg_list()
         return [platform_arg] + self._create_autotuner_rapids_args()
 
 
