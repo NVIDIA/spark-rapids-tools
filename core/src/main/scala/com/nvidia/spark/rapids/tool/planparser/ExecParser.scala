3fa718d1e65c9495e97b500cc7b2e5dc901ba1b7
Niranjan Artal <nartal@nvidia.com>
2024-04-09 19:17:27 -0700

    Add support for unsupported expressions reasons per Exec
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- core/src/main/scala/com/nvidia/spark/rapids/tool/planparser/ExecParser.scala
+++ core/src/main/scala/com/nvidia/spark/rapids/tool/planparser/ExecParser.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,10 @@
 
 package com.nvidia.spark.rapids.tool.planparser
 
+import org.apache.spark.sql.rapids.tool.UnsupportedExpr
+
 trait ExecParser {
   def parse: ExecInfo
+  def getUnsupportedExprReasonsForExec(expressions: Array[String]): Seq[UnsupportedExpr] = Seq.empty
   val fullExecName: String
 }
