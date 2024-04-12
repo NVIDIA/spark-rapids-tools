82d45cd254d8382965ad163ef005dc80444dc890
cindyyuanjiang <cindyj@nvidia.com>
2024-04-12 11:30:53 -0700

    removed estimation_model default
    Signed-off-by: cindyyuanjiang <cindyj@nvidia.com>

--- user_tools/src/spark_rapids_tools/cmdli/tools_cli.py
+++ user_tools/src/spark_rapids_tools/cmdli/tools_cli.py
@@ -23,7 +23,7 @@ from spark_rapids_tools.utils.util import gen_app_banner, init_environment
 from spark_rapids_pytools.common.utilities import Utils, ToolLogging
 from spark_rapids_pytools.rapids.bootstrap import Bootstrap
 from spark_rapids_pytools.rapids.profiling import ProfilingAsLocal
-from spark_rapids_pytools.rapids.qualification import QualEstimationModel, QualFilterApp, QualificationAsLocal
+from spark_rapids_pytools.rapids.qualification import QualFilterApp, QualificationAsLocal
 
 
 class ToolsCLI(object):  # pylint: disable=too-few-public-methods
@@ -41,7 +41,7 @@ class ToolsCLI(object):  # pylint: disable=too-few-public-methods
                       target_platform: str = None,
                       output_folder: str = None,
                       filter_apps: str = QualFilterApp.tostring(QualFilterApp.get_default()),
-                      estimation_model: str = QualEstimationModel.tostring(QualEstimationModel.get_default()),
+                      estimation_model: str = None,
                       tools_jar: str = None,
                       cpu_cluster_price: float = None,
                       estimated_gpu_cluster_price: float = None,
