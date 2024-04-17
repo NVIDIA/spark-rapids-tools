948a3448b63e86150806a8d16c7b414b3586efdc
cindyyuanjiang <cindyj@nvidia.com>
2024-04-17 15:08:48 -0700

    added a simple parser for batchevalpython exec
    Signed-off-by: cindyyuanjiang <cindyj@nvidia.com>

---
+++ core/src/main/scala/com/nvidia/spark/rapids/tool/planparser/BatchEvalPythonExecParser.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.spark.rapids.tool.planparser
+
+import com.nvidia.spark.rapids.tool.qualification.PluginTypeChecker
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.execution.ui.SparkPlanGraphNode
+
+case class BatchEvalPythonExecParser(
+    node: SparkPlanGraphNode,
+    checker: PluginTypeChecker,
+    sqlID: Long) extends ExecParser with Logging {
+
+  val fullExecName = node.name + "Exec"
+
+  override def parse: ExecInfo = {
+    val duration = None
+    // TODO - add in parsing expressions - average speedup across?
+    ExecInfo(node, sqlID, node.name, "", 1.0, duration, node.id, true, None)
+  }
+}
