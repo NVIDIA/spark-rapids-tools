577e4987c2515ab6a6b1af8b195066469a6658bb
cindyyuanjiang <cindyj@nvidia.com>
2024-05-15 14:22:21 -0700

    python style fix
    Signed-off-by: cindyyuanjiang <cindyj@nvidia.com>

--- user_tools/src/spark_rapids_tools/storagelib/adls/adlspath.py
+++ user_tools/src/spark_rapids_tools/storagelib/adls/adlspath.py
@@ -25,11 +25,11 @@ class AdlsPath(CspPath):
 
     @classmethod
     def get_abfs_account_name(cls, path: str) -> str:
-        return path.split('@')[1].split('.')[0]
+        return path.split("@")[1].split(".")[0]
 
     @classmethod
     def is_protocol_prefix(cls, value: str) -> bool:
-        if not 'AZURE_STORAGE_ACCOUNT_NAME' in os.environ:
+        if "AZURE_STORAGE_ACCOUNT_NAME" not in os.environ:
             account_name = cls.get_abfs_account_name(value)
-            os.environ['AZURE_STORAGE_ACCOUNT_NAME'] = account_name
+            os.environ["AZURE_STORAGE_ACCOUNT_NAME"] = account_name
         return super().is_protocol_prefix(value)
