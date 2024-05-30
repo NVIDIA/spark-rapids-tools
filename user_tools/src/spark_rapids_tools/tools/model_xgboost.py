58de46354c0624deee3425fb572e5d0c291a424a
Partho Sarthi <psarthi@nvidia.com>
2024-05-30 11:23:01 -0700

    Refactor to move inside condition
    Signed-off-by: Partho Sarthi <psarthi@nvidia.com>

--- user_tools/src/spark_rapids_tools/tools/model_xgboost.py
+++ user_tools/src/spark_rapids_tools/tools/model_xgboost.py
@@ -554,17 +554,18 @@ def load_csv_files(
     job_agg_tbl = scan_tbl('job_level_aggregated_task_metrics')
     stage_agg_tbl = scan_tbl('stage_level_aggregated_task_metrics')
 
-    # Rename jobId and stageId to ID
-    job_df = job_agg_tbl.rename(columns={'jobId': 'ID'})
-    job_df['ID'] = 'job_' + job_df['ID'].astype(str)
-    stage_df = stage_agg_tbl.rename(columns={'stageId': 'ID'})
-    job_df['ID'] = 'stage_' + stage_df['ID'].astype(str)
+    if not any([job_agg_tbl.empty, stage_agg_tbl.empty, job_map_tbl.empty]):
+        # Rename jobId and stageId to ID
+        job_df = job_agg_tbl.rename(columns={'jobId': 'ID'})
+        job_df['ID'] = 'job_' + job_df['ID'].astype(str)
+        stage_df = stage_agg_tbl.rename(columns={'stageId': 'ID'})
+        job_df['ID'] = 'stage_' + stage_df['ID'].astype(str)
 
-    # Concatenate the DataFrames.
-    # TODO: This is a temporary solution to minimize changes in existing code.
-    #        We should refactor this once we have updated the code with latest changes.
-    job_stage_agg_tbl = pd.concat([job_df, stage_df], ignore_index=True)
-    if not any([job_stage_agg_tbl.empty, job_map_tbl.empty]):
+        # Concatenate the DataFrames.
+        # TODO: This is a temporary solution to minimize changes in existing code.
+        #        We should refactor this once we have updated the code with latest changes.
+        job_stage_agg_tbl = pd.concat([job_df, stage_df], ignore_index=True)
+
         job_stage_agg_tbl = job_stage_agg_tbl.drop(columns='appIndex')
         job_stage_agg_tbl = job_stage_agg_tbl.rename(
             columns={'numTasks': 'numTasks_sum', 'duration_avg': 'duration_mean'}
