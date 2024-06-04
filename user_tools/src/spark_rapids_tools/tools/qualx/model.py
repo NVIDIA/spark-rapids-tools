b7f6cf784fb4858eae6191fa6975fda5f738a12f
Partho Sarthi <psarthi@nvidia.com>
2024-06-03 21:58:59 -0700

    Handle optional dependencies and add copyrights
    Signed-off-by: Partho Sarthi <psarthi@nvidia.com>

--- user_tools/src/spark_rapids_tools/tools/qualx/model.py
+++ user_tools/src/spark_rapids_tools/tools/qualx/model.py
@@ -1,6 +1,19 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Callable, List, Tuple
 import numpy as np
-import optuna
 import pandas as pd
 import random
 import shap
@@ -9,7 +22,11 @@ from spark_rapids_tools.tools.qualx.preprocess import expected_raw_features
 from spark_rapids_tools.tools.qualx.util import get_logger
 from tabulate import tabulate
 from xgboost import XGBModel
-
+# Import optional packages
+try:
+    import optuna
+except ImportError:
+    optuna = None
 
 logger = get_logger(__name__)
 
