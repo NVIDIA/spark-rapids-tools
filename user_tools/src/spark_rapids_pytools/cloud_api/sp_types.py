87da9fedcb2e9ad06268ff0d141df42b12ee20fb
Partho Sarthi <psarthi@nvidia.com>
2024-08-07 16:33:58 -0700

    Update string for GPU information
    Signed-off-by: Partho Sarthi <psarthi@nvidia.com>

--- user_tools/src/spark_rapids_pytools/cloud_api/sp_types.py
+++ user_tools/src/spark_rapids_pytools/cloud_api/sp_types.py
@@ -1314,10 +1314,10 @@ class ClusterBase(ClusterGetAccessor):
     def _get_gpu_conversion_str(self) -> str:
         """
         Returns a string representing the GPU configuration.
-        Example: '(with 2 T4)'
+        Example: '(1 L4 each)'
         """
         gpu_per_machine, gpu_device_str = self.get_gpu_per_worker()
-        return f'(with {gpu_per_machine} {gpu_device_str})'
+        return f'({gpu_per_machine} {gpu_device_str} each)'
 
 
 @dataclass
