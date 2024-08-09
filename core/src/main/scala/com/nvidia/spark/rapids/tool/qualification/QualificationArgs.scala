3a463658d404f0e36f1a9c9cb7c59b1aae62ddb2
Thomas Graves <tgraves@nvidia.com>
2024-08-09 16:38:48 -0500

    Add option to filter event logs for a maximum file system size
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>

--- core/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
+++ core/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
@@ -88,6 +88,13 @@ Usage: java -cp rapids-4-spark-tools_2.12-<version>.jar:$SPARK_HOME/jars/*
       descr = "Filter event logs whose application start occurred within the past specified " +
         "time period. Valid time periods are min(minute),h(hours),d(days),w(weeks)," +
         "m(months). If a period is not specified it defaults to days.")
+  val maxEventLogSize: ScallopOption[String] =
+    opt[String](required = false,
+      descr = "Process only application event logs whose size is less than or equal to the size " +
+        "specified. Valid units of size are b(bytes),k(kilobytes),m(metabytes),g(gigabytes). " +
+        "If no units are specified, the size is assumed to be mb. Note, this does not support " +
+        "event log rolling which puts multiple event logs for the same application into a " +
+        "single directory.")
   val matchEventLogs: ScallopOption[String] =
     opt[String](required = false,
       descr = "Filter event logs whose filenames contain the input string. Filesystem " +
