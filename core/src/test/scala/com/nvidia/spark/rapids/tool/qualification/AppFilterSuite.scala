c38d16c830e3ddca7c8604575020516c9b71342b
Thomas Graves <tgraves@nvidia.com>
2024-08-19 12:56:35 -0500

    add tests for start and end time filters

--- core/src/test/scala/com/nvidia/spark/rapids/tool/qualification/AppFilterSuite.scala
+++ core/src/test/scala/com/nvidia/spark/rapids/tool/qualification/AppFilterSuite.scala
@@ -238,14 +238,43 @@ class AppFilterSuite extends BaseTestSuite {
   }
 
   private val appsWithFsNewOlderToTest = Array(
-    TestEventLogFSAndAppNameInfo("ndshours18", msHoursAgo(18), 1),
-    TestEventLogFSAndAppNameInfo("ndsweeks2", msWeeksAgo(2), 1),
-    TestEventLogFSAndAppNameInfo("nds86", msDaysAgo(4), 1),
-    TestEventLogFSAndAppNameInfo("nds86", msWeeksAgo(2), 2))
+    TestEventLogFSAndAppNameInfo("feb01-00",
+      AppFilterImpl.parseDateTimePeriod("2024-02-01 00:00:00").get, 1),
+    TestEventLogFSAndAppNameInfo("feb02-07",
+      AppFilterImpl.parseDateTimePeriod("2024-02-02 07:12:11").get, 2),
+    TestEventLogFSAndAppNameInfo("feb02-15",
+      AppFilterImpl.parseDateTimePeriod("2024-02-02 15:00:00").get, 3),
+    TestEventLogFSAndAppNameInfo("may15-13",
+      AppFilterImpl.parseDateTimePeriod("2024-05-15 13:00:00").get, 4),
+  )
 
-  test("start ane end filesystem both") {
-    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2-oldest-filesystem",
-      "ndsweeks2", 1)
+  test("start and end filesystem both on all") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2024-02-01 00:00:00",
+      "2024-05-15 13:00:00", 4)
+  }
+
+  test("start and end filesystem both on some") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2024-02-00 10:00:00",
+      "2024-02-02 13:00:00", 2)
+  }
+
+  test("start filesystem some exact") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2024-02-02 07:12:11",
+      "", 3)
+  }
+
+  test("start filesystem none") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2024-05-15 15:00:00",
+      "", 0)
+  }
+  test("end filesystem some exact") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "2024-02-02 15:00:00",
+      "", 2)
+  }
+
+  test("end filesystem none") {
+    testFileSystemNewerAndOlderTimes(appsWithFsNewOlderToTest, "",
+      "2024-01-01 00:00:00", 0)
   }
 
   private def testFileSystemTimeAndStart(apps: Array[TestEventLogFSAndAppNameInfo],
@@ -298,15 +327,21 @@ class AppFilterSuite extends BaseTestSuite {
           new File(elogFile.toString).setLastModified(app.fsTime)
           elogFile.toString
         }
-
-        val allArgs = Array(
+        val startingArgs = Array(
           "--output-directory",
           outpath.getAbsolutePath(),
-          "--fs-start-time",
-          fsStartTime,
-          "--fs-end-time",
-          fsEndTime
         )
+        val argsWithOptStart = if (!fsStartTime.isEmpty) {
+          startingArgs ++ Array("--fs-start-time", fsStartTime)
+        } else {
+          startingArgs
+        }
+        val allArgs = if (!fsEndTime.isEmpty) {
+          argsWithOptStart ++ Array("--fs-end-time", fsEndTime)
+        } else {
+          argsWithOptStart
+        }
+
         val appArgs = new QualificationArgs(allArgs ++ fileNames)
         val (exit, appSum) = QualificationMain.mainInternal(appArgs)
         assert(exit == 0)
