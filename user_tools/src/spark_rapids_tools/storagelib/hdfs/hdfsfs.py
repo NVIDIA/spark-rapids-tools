8d82acd839bc9b90b50058a9ccd8c2884240ccba
Ahmed Hussein <ahussein@nvidia.com>
2024-08-22 14:20:40 -0500

    Update copyrights
    Signed-off-by: Ahmed Hussein <ahussein@nvidia.com>

--- user_tools/src/spark_rapids_tools/storagelib/hdfs/hdfsfs.py
+++ user_tools/src/spark_rapids_tools/storagelib/hdfs/hdfsfs.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 """Wrapper for the Hadoop File system"""
+
 from typing import Any
 
 from pyarrow.fs import HadoopFileSystem
