019ede2fdc87f109f895ce67161c506dd377d80a
Niranjan Artal <50492963+nartal1@users.noreply.github.com>
2024-11-06 09:20:27 -0800

    Refactor Exec Parsers - remove individual parser classes (#1396)
    * Refactor Exec Parsers - unify the code calling similar methods
    
    This PR is to remove redundant code in many ExecParsers. We have
    several ExecParsers with the same functionality in parse code.
    In this PR, following changes are done:
    
    1. If the Execs don't have expressions, then execName is assigned and
    ExecInfo object is created from GenericExecParser.
    2. If the Execs have expressions then reflection is used to assign the
    appropriate parser to be used.
    3. If there is any additional calculation in the parse function, then
    only that part is overriden in the ExecParser and the rest of the
    function uses the GenereicExecParser base code
    4.Removed all the files which are not needed anymore
    
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>
    
    * Remove reflection instead use case classes to match and call respective Parsers
    
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>
    
    * addressed few review comments
    
    1. default Exec name
    2. added argument to GenericParser constructor
    
    * addressed review comments
    
    * Update tests
    
    ---------
    
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

---
+++ core/src/main/scala/com/nvidia/spark/rapids/tool/planparser/GenericExecParser.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.spark.rapids.tool.planparser
+
+import com.nvidia.spark.rapids.tool.qualification.PluginTypeChecker
+
+import org.apache.spark.sql.execution.ui.SparkPlanGraphNode
+import org.apache.spark.sql.rapids.tool.{AppBase, UnsupportedExpr}
+
+class GenericExecParser(
+    val node: SparkPlanGraphNode,
+    val checker: PluginTypeChecker,
+    val sqlID: Long,
+    val execName: Option[String] = None,
+    val expressionFunction: Option[String => Array[String]] = None,
+    val app: Option[AppBase] = None
+) extends ExecParser {
+
+  val fullExecName: String = execName.getOrElse(node.name + "Exec")
+
+  override def parse: ExecInfo = {
+    val duration = computeDuration
+    val expressions = parseExpressions()
+    val notSupportedExprs = getNotSupportedExprs(expressions)
+    val isExecSupported = checker.isExecSupported(fullExecName) &&
+      notSupportedExprs.isEmpty
+
+    val (speedupFactor, isSupported) = if (isExecSupported) {
+      (checker.getSpeedupFactor(fullExecName), true)
+    } else {
+      (1.0, false)
+    }
+
+    createExecInfo(speedupFactor, isSupported, duration, notSupportedExprs)
+  }
+
+  protected def parseExpressions(): Array[String] = {
+    expressionFunction match {
+      case Some(func) =>
+        val exprString = getExprString
+        func(exprString)
+      case None =>
+        Array.empty[String]
+    }
+  }
+
+  protected def getExprString: String = {
+    node.desc.replaceFirst(s"${node.name} ", "")
+  }
+
+  protected def getNotSupportedExprs(expressions: Array[String]): Seq[UnsupportedExpr] = {
+    checker.getNotSupportedExprs(expressions)
+  }
+
+  protected def getDurationMetricIds: Seq[Long] = {
+    Seq.empty
+  }
+
+  protected def computeDuration: Option[Long] = {
+    // Sum the durations for all metrics returned by getDurationMetricIds
+    val durations = getDurationMetricIds.flatMap { metricId =>
+      app.flatMap(appInstance => SQLPlanParser.getTotalDuration(Some(metricId), appInstance))
+    }
+    durations.reduceOption(_ + _)
+  }
+
+  protected def createExecInfo(
+      speedupFactor: Double,
+      isSupported: Boolean,
+      duration: Option[Long],
+      notSupportedExprs: Seq[UnsupportedExpr]
+  ): ExecInfo = {
+    ExecInfo(
+      node,
+      sqlID,
+      node.name,
+      "",
+      speedupFactor,
+      duration,
+      node.id,
+      isSupported,
+      None,
+      unsupportedExprs = notSupportedExprs
+    )
+  }
+}
+
+object GenericExecParser {
+  def apply(
+      node: SparkPlanGraphNode,
+      checker: PluginTypeChecker,
+      sqlID: Long,
+      execName: Option[String] = None,
+      expressionFunction: Option[String => Array[String]] = None,
+      app: Option[AppBase] = None
+  ): GenericExecParser = {
+    val fullExecName = execName.getOrElse(node.name + "Exec")
+    new GenericExecParser(node, checker, sqlID, Some(fullExecName), expressionFunction, app)
+  }
+}
