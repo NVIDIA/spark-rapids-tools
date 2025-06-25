51c64aa8d2bbb3d94d7f24f9b522b516ce8f1b2c
Ahmed Hussein <50450311+amahussein@users.noreply.github.com>
2025-06-25 08:34:04 -0500

    Increase the throughput of scanning eventlogs in core-tools (#1738)
    Signed-off-by: Ahmed Hussein (amahussein) <a@ahussein.me>
    
    Fixes #1668
    
    This code change checks the beginning of each line before converting it
    to a JsonNode. This reduces the work done by each thread in parsing the
    eventlog lines.
    
    - Add a filter to the reader loop to examine the line
    - Defines a new configuration for eventlog parser
    - Adds ability to configure each tool and define the list of supported
    events.
    
    ---------
    
    Signed-off-by: Ahmed Hussein (amahussein) <a@ahussein.me>

---
+++ core/src/main/scala/org/apache/spark/sql/rapids/tool/EventLogParserTrait.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.rapids.tool
+
+import org.apache.spark.sql.rapids.tool.util.{EventUtils, ToolsMetric}
+
+/**
+ * Trait defining common methods, fields and metrics to process eventlog lines.
+ * Metrics counts number help us understand the overhead and the work done by the parser.
+ */
+trait EventLogParserTrait {
+  lazy private val linePrefixes =
+    EventUtils.getAcceptedLinePrefix(this.getClass.getSimpleName)
+  // Map between name and toolsMetrics.
+  // Track the number of skipped lines and number of parsed lines in the eventlog.
+  private val parserMetrics: Map[String, ToolsMetric] =
+    Map(
+      "skippedLines" ->
+        ToolsMetric("eventLog skippedLines",
+          "Number of lines skipped in an eventlog"),
+      "parsedLines" ->
+        ToolsMetric("eventLog parsedLines",
+          "Number of lines parsed in an eventlog"))
+
+  /**
+   * Check if the line starts with one of the accepted prefixes
+   * @param line the line extracted from the eventlog.
+   * @return true if the line starts with one of the accepted prefixes.
+   */
+  def acceptLine(line: String): Boolean = {
+    val result = linePrefixes.isEmpty || linePrefixes.exists(line.startsWith)
+    if (!result) {
+      parserMetrics("skippedLines").inc()
+    } else {
+      parserMetrics("parsedLines").inc()
+    }
+    result
+  }
+
+  // Define getters for metrics used to gather statistics
+  def getTotalLines: Long = parserMetrics.values.map(_.getValue).sum
+  def getProcessedLinesCount: Long = parserMetrics("parsedLines").getValue
+  def getSkippedLinesCount: Long = parserMetrics("skippedLines").getValue
+}
